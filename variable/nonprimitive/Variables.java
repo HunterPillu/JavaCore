23078572297fb4a42c22bca785c48fd844085ed2
Prinkal KUmar <gur54218>
2022-01-20 00:24:54 +0530

    typeofvar

---
+++ variable/nonprimitive/Variables.java
@@ -0,0 +1,31 @@
+//non premitive datatype
+
+
+public class Variables{
+	
+	public static void main(String[] aa){
+		
+		String s1;
+		
+		s1= "Full Sentence";
+		
+		System.out.println(s1);
+		
+		String s2 = "another sentence";
+		
+		String s3 = "";
+		
+		String s4 = null;
+		
+		String name ;
+		String address;
+		String description;
+		int pincode = 834001;
+		
+		System.out.println(s3.isEmpty());
+		
+		
+		
+		
+	}
+}
---
+++ variable/typesOfVariable/Variables.java
@@ -0,0 +1,46 @@
+public class Variables{
+	
+	static int gate =2;
+	
+	int watchman = 1;
+	int doctor =6;
+	
+	
+	public static void main(String[] args){
+		
+		
+		
+		int tv = 9;  //local variable for main method
+		
+		
+		System.out.println(tv);
+		
+		System.out.println(gate);
+		
+		Variables v = new Variables();
+		System.out.println(v.doctor);
+		
+		
+	}
+	
+	
+	
+	
+	public static void main2(){
+		
+		int k =99;
+		
+		System.out.println(gate);
+	}
+	
+	
+	
+	public static void main3(){
+		
+	}
+	
+	
+	public static void main4(){
+		
+	}
+}
