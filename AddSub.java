8a1b68efcafe605b803cfd01f5ce50877ba1d581
Ritesh Kumar Ram <riteshkr02224@gmail.com>
2022-02-02 16:37:00 +0530

    modified

--- AddSub.java
+++ AddSub.java
@@ -8,7 +8,7 @@ public class AddSub
 		d=b-a;
 		f=a*b;
 		g=b/a;
-		System.out.println("Additin =" + c + "\t" + d);
+		System.out.rintln("Additin =" + c + "\t" + d);
 		System.out.println("Substration ="+ d);
 		System.out.println("multiplication ="+ f);
 		System.out.println("division ="+g );
