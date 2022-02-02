610294b6c26a7c6f21e41017621b031fd8e394d6
Ritesh Kumar Ram <riteshkr02224@gmail.com>
2022-02-02 16:29:18 +0530

    addition

---
+++ AddSub.java
@@ -0,0 +1,17 @@
+public class AddSub
+{
+	public static void main(String ritz[])
+	{
+		int a=12, b=13,c,d;
+		float f,g;
+		c=a+b;
+		d=b-a;
+		f=a*b;
+		g=b/a;
+		System.out.println("Additin =" + c + "\t" + d);
+		System.out.println("Substration ="+ d);
+		System.out.println("multiplication ="+ f);
+		System.out.println("division ="+g );
+	}
+}
+		
